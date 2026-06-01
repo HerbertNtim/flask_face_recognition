2184e0c9a1fa4bfaa8f49dd2464987a4336c6dce
HerbertNtim <herbertntim15@gmail.com>
2026-06-01 17:09:35 +0000

    creating views

---
+++ app/view.py
@@ -0,0 +1,2 @@
+def index():
+  return 'Welcome to face recognition from Views'
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from app import view
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.add_url_rule(rule= '/', endpoint = 'home', view_func = view.index)
+
+if __name__ == '__main__':
+  app.run(debug=True)
