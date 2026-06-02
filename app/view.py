b21d752e8ad0b583549b102792c935a4f7b42a3d
HerbertNtim <herbertntim15@gmail.com>
2026-06-02 11:02:10 +0000

    application page

--- app/view.py
+++ app/view.py
@@ -2,3 +2,6 @@ from flask import render_template
 
 def index():
   return render_template('index.html')
+
+def app():
+  return render_template('app.html')
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import view
 app = Flask(__name__)
 
 app.add_url_rule(rule= '/', endpoint = 'home', view_func = view.index)
+app.add_url_rule(rule= '/app', endpoint = 'app', view_func = view.app)
 
 if __name__ == '__main__':
   app.run(debug=True)
