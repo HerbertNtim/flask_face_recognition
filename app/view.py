1a4bd71529c3a7ebf87fdee58602507bf33e0cc9
HerbertNtim <herbertntim15@gmail.com>
2026-06-02 17:11:41 +0000

    made prediction

--- app/view.py
+++ app/view.py
@@ -3,7 +3,8 @@ import cv2
 from app.face_recognition import faceRecognitionPipeline
 from flask import render_template, request
 
-UPLOAD_FOLDER = 'static/upload'
+UPLOAD_FOLDER = os.path.join('static', 'upload')
+PREDICT_FOLDER = os.path.join('static', 'predict')
 
 def index():
   return render_template('index.html')
@@ -15,11 +16,20 @@ def gender():
   if request.method == 'POST':
     f = request.files['image_name']
     filename = f.filename
+    
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    os.makedirs(PREDICT_FOLDER, exist_ok=True)
+    
     # Save image in upload folder
     path = os.path.join(UPLOAD_FOLDER, filename)
     f.save(path)   
     # Get predictions
     pred_image, predictions = faceRecognitionPipeline(path)
+    pred_filename = 'prediction_image.jpg'
+    pred_path = os.path.join(PREDICT_FOLDER, pred_filename)
+    
+    cv2.imwrite(pred_path, pred_image)
+    
     print('ML model predicted successfully')
     
     
